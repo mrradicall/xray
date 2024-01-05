1dc543fb3465d83e8dd5e732300d82e7cd3d1033
Radical <mrradical755@gmail.com>
2024-01-05 11:53:51 +0330

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -135,7 +135,7 @@ install_x-ui() {
         fi
     else
         last_version="v1.7.9"
-        url="https://github.com/MHSanaei/3x-ui/releases/download/v1.7.9/x-ui-linux-$(arch3xui).tar.gz"
+        url="https://github.com/MHSanaei/3x-ui/releases/download/v2.0.2/x-ui-linux-$(arch3xui).tar.gz"
         echo -e "Begining to install x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz ${url}
         if [[ $? -ne 0 ]]; then
